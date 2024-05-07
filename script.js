0176f96dbaf34b5e8aff49bb8b05acb8de611df8
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-05-07 13:56:34 -0300

    Add a square grid of fixed size and flexibe dimension, determined by the variable gridDimension

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
+let gridDimension = 16;
+
+for (let i = 0; i < gridDimension; i++) {
     
     let verticalDiv = document.createElement('div');
     verticalDiv.classList.add('vertical');
@@ -8,13 +10,9 @@ for (let i = 0; i < 16; i++) {
     let text = document.createElement('p');
     text.textContent = 'test'
     
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < gridDimension; j++) {
         let horizontalDiv = document.createElement('div');
         horizontalDiv.classList.add('horizontal');
-        
-        let text = document.createElement('p');
-        text.textContent = 'test'
-        horizontalDiv.appendChild(text);
         
         verticalDiv.appendChild(horizontalDiv);
     }
