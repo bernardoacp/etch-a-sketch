3c8d9c38a4f775ba69c755296d2ca0671656c362
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2024-05-07 19:50:12 -0300

    Add a button to prompt the user for a grid size, then remove the current grid and create a new one of prompted size. Create some functions to perform methods already implemented (creating the div, painting the squares and removing the div), so that they can be reused.

--- script.js
+++ script.js
@@ -1,28 +1,76 @@
 let container = document.querySelector('#container');
 
-let gridDimension = 16;
+let setDimension = document.querySelector('.dimension');
 
-for (let i = 0; i < gridDimension; i++) {
+let pixel, pixelRow;
+let i, j;
+
+function createGrid(dimension) {
+    for (i = 0; i < dimension; i++) {
     
-    let verticalDiv = document.createElement('div');
-    verticalDiv.classList.add('vertical');
-    
-    let text = document.createElement('p');
-    text.textContent = 'test'
-    
-    for (let j = 0; j < gridDimension; j++) {
-        let horizontalDiv = document.createElement('div');
-        horizontalDiv.classList.add('horizontal');
+        let verticalDiv = document.createElement('div');
+        verticalDiv.classList.add('vertical');
         
-        verticalDiv.appendChild(horizontalDiv);
+        let text = document.createElement('p');
+        text.textContent = 'test'
+        
+        for (j = 0; j < dimension; j++) {
+            let horizontalDiv = document.createElement('div');
+            horizontalDiv.classList.add('horizontal');
+            
+            verticalDiv.appendChild(horizontalDiv);
+        }
+        container.appendChild(verticalDiv);
     }
-    container.appendChild(verticalDiv);
 }
 
-let pixel = document.querySelectorAll('.horizontal')
+function paintGrid() {
+    pixel = document.querySelectorAll('.horizontal');
+    pixelRow = document.querySelectorAll('.vertical');
 
-for (let i = 0; i < pixel.length; i++) {
-    pixel[i].addEventListener('mouseover', (event2) => {
-        event2.target.style.backgroundColor = 'black';
-    } )
+    for (i = 0; i < pixel.length; i++) {
+        pixel[i].addEventListener('mouseover', (event2) => {
+            event2.target.style.backgroundColor = 'black';
+        } )
+    }
+}
+
+function clearGrid() {
+    for (i = 0; i < pixel.length; i++)
+        pixel[i].remove();
+    
+    for (i = 0; i < pixelRow.length; i++) 
+        pixelRow[i].remove();
+}
+
+createGrid(16);
+paintGrid();
+
+setDimension.addEventListener('click', promptUser);
+
+function promptUser() {
+    gridDimension = window.prompt('Please enter the number of squares per side:', '16');
+
+    if (gridDimension == undefined)
+        return;
+
+    if (isNaN(gridDimension)) {
+        alert('Invalid input');
+        return;
+    }
+
+    gridDimension = parseInt(gridDimension);
+    
+    if (gridDimension > 100) {
+        alert('Maximum dimension is 100');
+        return;
+    }
+    if (gridDimension <= 0) {
+        alert('Invalid dimension');
+        return;
+    }
+
+    clearGrid();
+    createGrid(gridDimension);
+    paintGrid();
 }
